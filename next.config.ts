a793a5109b43b3547a276784a7378f112b9da1da
fedel <fedelewu@outlook.com>
2025-09-09 18:28:48 -0700

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,36 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    webpack: (config, context) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        })
+        config.module.rules.push({
+            test: /\.glsl/,
+            use: [
+                context.defaultLoaders.babel,
+                {
+                    loader: "raw-loader"
+                }
+            ]
+        })
+        return config;
+    },
+    turbopack: {
+        root: "./",
+        rules: {
+            "*.svg": {
+                loaders: ["@svgr/webpack"],
+                as: "*.js"
+            },
+            "*.glsl": {
+                loaders: ['raw-loader'],
+                as: "*.js"
+            }
+        }
+    }
 };
 
 export default nextConfig;
